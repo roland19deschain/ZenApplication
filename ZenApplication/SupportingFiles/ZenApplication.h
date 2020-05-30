abf0bfb1c69ec5d69db4087ca1a2d150f4d36944
Alexey Roik <alexey.roik@nebulagames.org>
2020-05-30 22:19:48 +0300

    Add empty cocoa touch framework application

---
+++ ZenApplication/SupportingFiles/ZenApplication.h
@@ -0,0 +1,19 @@
+//
+//  ZenApplication.h
+//  ZenApplication
+//
+//  Created by Alexey Roik on 5/30/20.
+//  Copyright © 2020 Nebula. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ZenApplication.
+FOUNDATION_EXPORT double ZenApplicationVersionNumber;
+
+//! Project version string for ZenApplication.
+FOUNDATION_EXPORT const unsigned char ZenApplicationVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ZenApplication/PublicHeader.h>
+
+
