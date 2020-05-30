8e59236a87f18e08ca572f68026a5f91bf679c30
Alexey Roik <alexey.roik@nebulagames.org>
2020-05-31 00:20:24 +0300

    Add 'ListViewModel'

---
+++ ZenApplication/SourceCode/ViewModels/List/ListViewModel.swift
@@ -0,0 +1,27 @@
+import Foundation
+
+public struct ListViewModel<RowModel> {
+	
+	// MARK: - Subscriptions
+	
+	public subscript(index: Int) -> RowModel? {
+		rows[safe: index]
+	}
+	
+	// MARK: - Computed Properties
+	
+	public var count: Int {
+		rows.count
+	}
+	
+	// MARK: - Stored Properties
+	
+	private let rows: [RowModel]
+	
+	// MARK: - Life Cycle
+	
+	public init(rows: [RowModel]) {
+		self.rows = rows
+	}
+	
+}
