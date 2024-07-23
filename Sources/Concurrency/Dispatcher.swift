570ad2fac9266c09a25088087f8536fc3cacd9a6
Alexey Roik <roland19deschain@gmail.com>
2024-07-24 01:25:52 +0300

    Update 'Dispatcher'

--- Sources/Concurrency/Dispatcher.swift
+++ Sources/Concurrency/Dispatcher.swift
@@ -1,34 +1,54 @@
 import Dispatch
+import ZenSwift
 
 public struct Dispatcher {
 	
 	// MARK: - Stored Properties
 	
-	private static var onceTokens: [String] = []
+	private static var objects: [Weak<AnyObject>] = []
+	private static var tokens: [String] = []
 	private static let dispatchSemaphore = DispatchSemaphore(value: 1)
 	
 	// MARK: - Dispatch
 	
-	/**
-	 Executes a block of code, associated with a unique token, only once. The code is thread safe and will
-	 only execute the code once even in the presence of multithreaded calls.
-	 - parameter token: A unique reverse DNS style name or a GUID
-	 - parameter block: Block to execute once
-	 */
+	/// Executes a `block` of code, associated with a unique `token`, only once. The code is
+	/// thread safe and will only execute the code once even in the presence of multithreaded calls.
+	/// - Parameters:
+	///   - token: A unique reverse DNS style name or a GUID
+	///   - block: Block to execute once
 	public static func executeOnce(
 		token: String,
 		block: () -> Void
 	) {
 		dispatchSemaphore.wait()
-		
 		defer {
 			dispatchSemaphore.signal()
 		}
-		guard !onceTokens.contains(token) else {
+		guard !tokens.contains(token) else {
 			return
 		}
-		onceTokens.append(token)
-		
+		tokens.append(token)
+		block()
+	}
+	
+	/// Executes a `block` of code, associated with an `object`, only once. The code is
+	/// thread safe and will only execute the code once even in the presence of multithreaded calls.
+	/// - Parameters:
+	///   - object: An object the address of which will be used to check uniqueness
+	///   - block: Block to execute once
+	public static func executeOnce(
+		for object: AnyObject,
+		block: () -> Void
+	) {
+		dispatchSemaphore.wait()
+		defer {
+			dispatchSemaphore.signal()
+		}
+		objects.cleanUp()
+		guard !objects.contains(object) else {
+			return
+		}
+		objects.append(Weak(object))
 		block()
 	}
 	
