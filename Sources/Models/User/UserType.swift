b7f43652e8e0c521fa0eb9c4b5bbe1c8e6101f1f
Alexey Roik <roland19deschain@gmail.com>
2026-04-21 15:22:54 +0300

    Update components

---
+++ Sources/Models/User/UserType.swift
@@ -0,0 +1,34 @@
+import Foundation
+
+public enum UserType: String, CaseIterable, Sendable, Equatable {
+	case regular
+	case subscriber
+	case premium
+}
+
+// MARK: - Convenience
+
+public extension UserType {
+	
+	var isRegular: Bool {
+		self == .regular
+	}
+	
+	var isSubscriber: Bool {
+		self == .subscriber
+	}
+	
+	var isPremium: Bool {
+		self == .premium
+	}
+	
+	var isPayer: Bool {
+		switch self {
+		case .regular:
+			false
+		case .subscriber, .premium:
+			true
+		}
+	}
+	
+}
